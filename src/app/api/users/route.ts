02227df8cc7774676363fff84097bf14ed1828cb
Kaming <lokaming86@gmail.com>
2025-10-31 13:26:34 +0700

    setup mongodb succesfully

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,11 +1,44 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
+import { connectDB } from "@/lib/db";
+import User from "@/models/user";
 
-export async function GET(req: Request) {
-  const users = [
-    { id: 1, name: 'Kaming', email: 'kaming@example.com' },
-    { id: 2, name: 'Budi', email: 'budi@example.com' },
-    { id: 3, name: 'Sari', email: 'sari@example.com' },
-  ];
+export async function GET() {
+  try {
+    await connectDB();
+    const users = await User.find();
 
-  return NextResponse.json({ users });
+    return NextResponse.json(users);
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.message || "Failed to fetch users" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    await connectDB();
+    const body = await req.json();
+    const { username, email, password, role} = body;
+
+    if (!username || !email || !password) {
+      return NextResponse.json(
+        { error: "username, email, and password are required" },
+        { status: 400 }
+      );
+    }
+
+    const user = await User.create({ username, email, password});
+
+    return NextResponse.json(
+      { message: "User created successfully", user },
+      { status: 201 }
+    );
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.message || "Failed to create user" },
+      { status: 500 }
+    );
+  }
 }
---
+++ src/models/user.ts
@@ -0,0 +1,20 @@
+import { Schema, model, models, Document } from "mongoose";
+
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  role?: 'admin' | 'user';
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    email: {type: String, required: true, unique: true},
+    role: { type: String, enum: ["admin", "user"], default: "user" },
+  },
+  { timestamps: true }
+);
+
+export default models.User || model<IUser>("User", userSchema);
